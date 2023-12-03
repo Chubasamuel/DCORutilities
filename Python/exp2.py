3c929416e8297e423d23766c562ba191a0ec9691
Chuba Samuel <chubasamuel@gmail.com>
2023-12-03 17:19:18 +0100

    Improved expenses analyser script

--- Python/exp2.py
+++ Python/exp2.py
@@ -1,19 +1,35 @@
 import xlrd as x
 from datetime import datetime as dt
+import json
 
-book=x.open_workbook("./expenses.xls")
+cyan = "\33[96m"
+bold = "\33[1m"
+red = "\33[91m"
+green = "\33[32m"
+cend = "\033[m"
 
+base_path = "../../../../Documents/exp"
+book=x.open_workbook(base_path+"/expenses.xls")
 sh=book.sheet_by_index(0)
 em={}
-fromDate = input("Enter start date in format dd-mm-yyyy\n\t")
-toDate = input("Enter end date in format dd-mm-yyyy\n\t")
+
+config = {}
+with open(base_path+"/config.json", "r") as f:
+    config = json.load(f)
+
+
+#fromDate = input("Enter start date in format dd-mm-yyyy\n\t")
+#toDate = input("Enter end date in format dd-mm-yyyy\n\t")
 
 indexes={"date":0,"purpose":1,"income":2,"expense":3,"category":4,"description":5}
 
-d,m,y=[int(i) for i in fromDate.split("-")]
-d2,m2,y2=[int(i) for i in toDate.split("-")]
-
-def invCategOrPurp(categOrPurp,isCateg,inc):
+def invCategOrPurp(categOrPurpM,isCateg,inc, dd,mm,yy,dd2,mm2,yy2):
+    ccCategP = categOrPurpM.split(":")
+    categOrPurp = ccCategP[0]
+    shouldMergeKey=False
+    if(len(ccCategP)>1 and ccCategP[1].strip()=="m"):
+        shouldMergeKey=True
+    
     res = {}
     incOrExp=2
     catOrP=1
@@ -31,7 +47,7 @@ def invCategOrPurp(categOrPurp,isCateg,inc):
             cD,cM,cY=[int(i) for i in str(dddd).split("-")]
         except:
             continue
-        if withinDate(cD,cM,cY):
+        if withinDate(cD,cM,cY,dd,mm,yy,dd2,mm2,yy2):
             v=sh.cell_value(rowx=r,colx=catOrP)
             cV=sh.cell_value(rowx=r,colx=incOrExp)
             if categOrPurp.lower()==v.lower():
@@ -41,7 +57,10 @@ def invCategOrPurp(categOrPurp,isCateg,inc):
             elif categOrPurp.lower() in v.lower():
                 if cV=="" or str(cV).strip()=="":
                     continue
-                putOrUp(res,v,cV)
+                if(shouldMergeKey):
+                    putOrUp(res,categOrPurp,cV)
+                else:
+                    putOrUp(res,v,cV)
     return res
 
 
@@ -50,13 +69,24 @@ def putOrUp(dic,key,val):
         dic[key]+=val
     else:
         dic[key]=val
-def withinDate(dd,mm,yyyy):
-    t=dt(yyyy,mm,dd).timestamp()
-    t1=dt(y,m,d).timestamp()
-    t2=dt(y2,m2,d2).timestamp()
+def withinDate(cD,cM,cY,dd,mm,yy,dd2,mm2,yy2):
+    t=dt(cY,cM,cD).timestamp()
+    t1=dt(yy,mm,dd).timestamp()
+    t2=dt(yy2,mm2,dd2).timestamp()
     return t>=t1 and t<=t2
+for c in config:
+    d,m,y=[int(i) for i in c["start"].split("-")]
+    d2,m2,y2=[int(i) for i in c["end"].split("-")]
+    params = c["params"].split(",")
+    isCateg = c["isCateg"]
+    isInc = c["isInc"]
+    res = [invCategOrPurp(i.strip(),isCateg,isInc, d,m,y,d2,m2,y2) for i in params]
+    print(bold,green,"from: ",cend,c["start"])
+    print(bold,green,"to: ",cend, c["end"])
+    print(bold,res,cend)
+    print("\n\n")
 
-isCateg=True
+"""isCateg=True
 isInc=False
 while True:
     inp=input("Is it a category? (Y/N)\t")
@@ -79,7 +109,7 @@ while True:
     else:
         continue
 catOrP=input("Enter names of category or purpose separated by comma:\n\t")
-catOrP=catOrP.split(",")
+catOrP=catOrP.split(",")"""
 
 """inv=["airtime","data","water"]
 for i in inv:
@@ -90,5 +120,5 @@ for i in inv:
 for i in inv:
     print(invCategOrPurp(i,isCateg,isInc))"""
 
-for c in catOrP:
-    print(invCategOrPurp(c.strip(),isCateg,isInc))
+"""for c in catOrP:
+    print(invCategOrPurp(c.strip(),isCateg,isInc))"""
